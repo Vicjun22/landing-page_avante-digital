0927d3357c51b1e9f6e1627d2ceb61fbd1899d48
Victor Ross <vicjun22@gmail.com>
2023-08-23 00:04:57 -0300

    WhatsApp

--- web/src/components/button.component.tsx
+++ web/src/components/button.component.tsx
@@ -4,7 +4,11 @@ interface ButtonProperties {
 
 export function Button({ children }: ButtonProperties) {
     return (
-        <a href="" type="submit" className="w-auto p-4 text-base md:text-base lg:text-base xl:text-lg font-extrabold text-[#fafafa] rounded-xl bg-[#0DC806]">
+        <a  target="_blank"
+            href="https://api.whatsapp.com/send?phone=5551991637205&amp;text=Olá, quero decolar minhas redes sociais agora!"
+            rel="nofollow noopener noreferrer"
+            type="submit"
+            className="w-auto p-4 text-base md:text-base lg:text-base xl:text-lg font-extrabold text-[#fafafa] rounded-xl bg-[#0DC806]">
             { children.toUpperCase() }
         </a>
     )
