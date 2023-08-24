37c92d1b1d77bac5d874c3e44f8a8c3e7b533e8c
Victor Ross <vicjun22@gmail.com>
2023-08-24 13:34:34 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     swcMinify: false,
-    distDir: 'out', // Defina explicitamente o diretório de saída
 }
 
 module.exports = nextConfig
