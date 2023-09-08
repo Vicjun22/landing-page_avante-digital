922cd2da87d9074ccf20967c307ae0890493488e
Victor Ross <vicjun22@gmail.com>
2023-09-08 15:13:11 -0300

    redirecionamento

--- src/components/buttonScrollToFrom.component.tsx
+++ src/components/buttonScrollToFrom.component.tsx
@@ -5,10 +5,10 @@ interface ButtonProperties {
 export function ButtonScrollToForm({ children }: ButtonProperties) {
 
     return (
-        <button title={ children }
-            // onClick={scrollToForm}
+        <a title={ children }
+            href="#formulario-online"
             className="w-auto p-4 text-base md:text-base lg:text-base xl:text-lg font-extrabold text-[#FAFAFA] rounded-xl bg-[#2E9A2A] hover:bg-[#0DC806]">
             { children.toUpperCase() }
-        </button>
+        </a>
     )
 }
