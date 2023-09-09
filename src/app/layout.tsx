bdd3de689337c0123aa468e28f7e4cb3b7bef467
Victor Ross <vicjun22@gmail.com>
2023-09-09 19:17:47 -0300

    Adicionando pixel do facebook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,24 @@ export default function RootLayout({
         <meta httpEquiv="Content-Language" content="pt-BR" />
         <meta name="keywords" content="web, avante.digital, react, next, agencia, agência, marketing, digital, marketing digital, agência de marketing digital" />
         <link rel="icon" type="image/jpeg" href='/avante-digital.jpg' sizes="any" />
+
+        { /* Meta Pixel Code */ }
+        <script dangerouslySetInnerHTML={{ __html:
+            `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3118022051838271');
+            fbq('track', 'PageView');`,
+        }} />
+      <noscript>
+        <img style={{ display: 'none' }} height="1" width="1"
+          src="https://www.facebook.com/tr?id=3118022051838271&ev=PageView&noscript=1" />
+      </noscript>
+        { /* End Meta Pixel Code */}
       </head>
 
       <body className={inter.className}>
--- src/components/buttonToFrom.component.tsx
+++ src/components/buttonToFrom.component.tsx
@@ -6,7 +6,7 @@ export function ButtonToForm({ children }: ButtonProperties) {
 
     return (
         <a title={ children }
-            href="https://oogsewrf5a4.typeform.com/to/tgel8f9j?typeform-source=www.encurtador.dev"
+            href="https://form.respondi.app/3KOh5AlK"
             className="w-auto p-4 text-base md:text-base lg:text-base xl:text-lg font-extrabold text-[#FAFAFA] rounded-xl bg-[#2E9A2A] hover:bg-[#0DC806]">
             { children.toUpperCase() }
         </a>
