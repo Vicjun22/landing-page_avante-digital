30cd1e5c588ceb705fa7ec4b4040de2bbd5845f5
Victor Elias Ross Júnior <vicjun22@gmail.com>
2023-12-18 21:20:57 -0300

    Alterando link do formulário

--- src/components/buttonToFrom.component.tsx
+++ src/components/buttonToFrom.component.tsx
@@ -6,7 +6,7 @@ export function ButtonToForm({ children }: ButtonProperties) {
 
     return (
         <a title={ children }
-            href="https://form.respondi.app/3KOh5AlK"
+            href="https://form.respondi.app/5sdxktDS"
             className="w-auto p-4 text-base md:text-base lg:text-base xl:text-lg font-extrabold text-[#FAFAFA] rounded-xl bg-[#2E9A2A] hover:bg-[#0DC806]">
             { children.toUpperCase() }
         </a>
